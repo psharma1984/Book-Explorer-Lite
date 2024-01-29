a2e9cfa754169d34118f989d841e5fae51fc05b4
psharma1984 <psharma.cad@gmail.com>
2024-01-29 14:30:35 -0800

    boilerplate code for app.js

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+require("express-async-errors");
+
+const app = express()
+
+require('dotenv').config();
+
+//routes
+app.get('/', (req, res) => {
+    res.send('Hello, Book Explorer!');
+});
+
+//error handling middleware
+app.use((req, res) => {
+    res.status(404).send(`That page (${req.url}) was not found.`);
+});
+
+app.use((err, req, res, next) => {
+    res.status(500).send(err.message);
+    console.log(err);
+});
+
+
+// Start the server
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+    try {
+        app.listen(port, () =>
+            console.log(`Server is listening on port ${port}...`)
+        );
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start();
