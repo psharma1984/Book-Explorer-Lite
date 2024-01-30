f8bf7e8f6a0a92b32958a229819e599f3966b3d9
psharma1984 <psharma.cad@gmail.com>
2024-01-30 14:28:23 -0800

    implemented crude bookdetail page

--- routes/Book.js
+++ routes/Book.js
@@ -12,6 +12,5 @@ router.route('/').get(bookList);
 router.route('/favorites/:id').post(addTofavorites)
 router.route('/:id').get(bookDetail)
 // Route to display detailed information about a selected book
-router.get('/books/:id',);
 
 module.exports = router;
