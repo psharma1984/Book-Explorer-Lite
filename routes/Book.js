c614de799fbeee75ba97a940f315caa2d06264cf
psharma1984 <psharma.cad@gmail.com>
2024-01-30 08:34:57 -0800

    corrected routes to addFavorites

--- routes/Book.js
+++ routes/Book.js
@@ -4,11 +4,12 @@ const Book = require('../models/Book');
 
 const {
     bookDetail,
-    bookList
+    bookList,
+    addTofavorites
 } = require('../controllers/Book');
 
 router.route('/').get(bookList);
-router.route('/favorites/:id')
+router.route('/favorites/:id').post(addTofavorites)
 router.route('/:id').get(bookDetail)
 // Route to display detailed information about a selected book
 router.get('/books/:id',);
